cec0caf9ee5789df41883aa1501c0db6de15c3c2
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-09 22:20:22 +0530

    setup login route and auth

--- api/index.js
+++ api/index.js
@@ -3,13 +3,20 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 
 dotenv.config();
 const salt = bcrypt.genSaltSync(10);
+const secret = "fakldsjfldsk1232kjzlsdjf";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 mongoose
@@ -32,6 +39,30 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (error) {
     res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    // log in
+    jwt.sign(
+      {
+        username,
+        id: userDoc._id,
+      },
+      secret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json("ok");
+      }
+    );
+  } else {
+    res.status(400).json("wrong credentials");
   }
 });
 
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,53 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function login(e) {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("Unable to login");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
-    <form className="login">
+    <form className="login" onSubmit={login}>
       <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <button>Login</button>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
     </form>
   );
 }

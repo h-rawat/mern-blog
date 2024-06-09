7bcd856a5f16c196c96e57f4f275a5cd11f0af0e
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-09 18:26:56 +0530

    create login and register page layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<div>Register Page</div>} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );

2fdac06069ca4c71d20e4007e2c14af81f992815
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-10 23:16:26 +0530

    get all posts from the DB and display in UI

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,28 +1,27 @@
-export default function Post() {
+import { formatISO9075 } from "date-fns";
+
+export default function Post({
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
   return (
     <div className="post">
       <div className="image">
-        <img
-          src="https://techcrunch.com/wp-content/uploads/2022/10/Acer-Chromebook-516-GE_rear-left-facing.jpeg"
-          alt=""
-        />
+        <img src={`http://localhost:4000/${cover}`} alt="" />
       </div>
       <div className="texts">
-        <h2>Google adds AI-powered features to Chromebook</h2>
+        <h2>{title}</h2>
         <p className="info">
           <a className="author" href="">
-            Name
+            {author.username}
           </a>
-          <time>2023-01-06 16:45</time>
+          <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
-        <p className="summary">
-          Google announced new AI-powered features today for its Chromebook Plus
-          line of devices, such as a writing assistant, a wallpaper creator and
-          easy access to Google’s Gemini chatbot. As a reminder, Chromebook Plus
-          laptops are productivity-focused devices with minimum spec
-          requirements. The company also unveiled new Chromebook Plus models
-          from HP, Acer and Asus.
-        </p>
+        <p className="summary">{summary}</p>
       </div>
     </div>
   );
--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import Post from "../Post";
 
 export default function IndexPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/post").then((res) => {
+      res.json().then((posts) => {
+        setPosts(posts);
+      });
+    });
+  }, []);
+
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
+      {posts.length &&
+        posts.map((post) => {
+          return <Post {...post} key={post._id} />;
+        })}
     </>
   );
 }

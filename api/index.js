59c0de6565459d518465fdb329fab18f1b8608b1
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-10 21:04:29 +0530

    create post api and integrate in UI

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const User = require("./models/User");
+const Post = require("./models/Post");
+const multer = require("multer");
+const fs = require("fs");
+const uploadMiddleware = multer({ dest: "uploads/" });
 
 dotenv.config();
 const salt = bcrypt.genSaltSync(10);
@@ -86,6 +90,26 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+
+  fs.renameSync(path, newPath);
+
+  const { title, summary, content } = req.body;
+
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  });
+
+  res.json(postDoc);
 });
 
 app.listen(4000);
